0ba29bb79beaa9f36ad58e74a1c5d61df548eb63
Manthan Mallikarjun <nahtnam@gmail.com>
2017-09-08 23:27:11 -0700

    fix(lib): only import lib if the directory exists

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 const requireDirectory = require('require-directory');
 
 const currentDirectory = process.cwd();
 
-module.exports = requireDirectory(module, path.join(currentDirectory, './lib'));
+let lib;
+const libPath = path.join(currentDirectory, './lib');
+if (fs.existsSync(libPath)) {
+  lib = requireDirectory(module, libPath);
+} else {
+  lib = {};
+}
+
+module.exports = lib;
