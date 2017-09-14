32365a88420862ac1dde8b3a19de1ef06545ed2f
Manthan Mallikarjun <nahtnam@gmail.com>
2017-09-13 19:31:50 -0700

    fix(lib): fix how lib is imported to stop circular dependencies

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,14 @@
 const path = require('path');
-const fs = require('fs');
-const requireDirectory = require('require-directory');
 
 const currentDirectory = process.cwd();
-
-let lib;
 const libPath = path.join(currentDirectory, './lib');
-if (fs.existsSync(libPath)) {
-  lib = requireDirectory(module, libPath);
-} else {
-  lib = {};
-}
 
-module.exports = lib;
+module.exports = (name) => {
+  let lib;
+  try {
+    lib = require(path.join(libPath, `${name}.js`));
+  } catch (_) {
+    // do nothing
+  }
+  return lib;
+};
