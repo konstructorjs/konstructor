5da63eb3f24158c7794afcd997f2c66f5747b050
Manthan Mallikarjun <nahtnam@gmail.com>
2017-09-14 15:19:36 -0700

    fix(import): fix how libs and models are imported to show errors

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,16 @@
 const path = require('path');
+const fs = require('fs');
 
 const currentDirectory = process.cwd();
 const libPath = path.join(currentDirectory, './lib');
 
 module.exports = (name) => {
-  let lib;
-  try {
-    lib = require(path.join(libPath, `${name}.js`));
-  } catch (_) {
-    // do nothing
+  const libFilePath = path.join(libPath, `${name}.js`);
+
+  if (fs.existsSync(libFilePath)) {
+    const lib = require(libFilePath);
+    return lib;
   }
-  return lib;
+
+  throw new Error(`lib ${libFilePath} does not exist`);
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,16 @@
 const path = require('path');
+const fs = require('fs');
 
 const currentDirectory = process.cwd();
 const modelsPath = path.join(currentDirectory, './app/models');
 
 module.exports = (name) => {
-  let model;
-  try {
-    model = require(path.join(modelsPath, `${name}.js`));
-  } catch (_) {
-    // do nothing
+  const modelFilePath = path.join(modelsPath, `${name}.js`);
+
+  if (fs.existsSync(modelFilePath)) {
+    const model = require(modelFilePath);
+    return model;
   }
-  return model;
+
+  throw new Error(`model ${modelFilePath} does not exist`);
 };
